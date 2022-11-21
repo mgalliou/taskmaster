1fb9d1bf48cbaeaa500ba555a2244b390fbff530
mgalliou <matthias.galliou@gmail.com>
2022-11-21 17:19:13 +0100

    refactor get_prog_conf

--- src/config.rs
+++ src/config.rs
@@ -14,21 +14,30 @@ pub struct Config {
     pub programs: HashMap<String, ProgramConfig>
 }
 
+fn get_field(prog: (&Yaml, &Yaml), field: &str) -> String {
+    match prog.1[field].as_str() {
+        Some(s) => s.to_string(),
+        None => panic!("field `{}` no found", field)
+    }
+}
+
+fn get_name(prog: (&Yaml, &Yaml)) -> String {
+    match prog.0.as_str() {
+        Some(s) => s.to_string(),
+        None => panic!("field is not as string"),
+    }
+}
+
 fn get_prog_conf(yaml: &Yaml) -> HashMap<String, ProgramConfig> {
     let mut prog_conf: HashMap<String, ProgramConfig> = HashMap::new();
-    let programs = &yaml["programs"];
+    let programs = yaml["programs"].as_hash().expect("no program field found");
 
-    for prog in programs.as_hash().into_iter() {
-        for e in prog.into_iter() {
-            let name = match e.0.as_str() {
-                Some(s) => s.to_string(),
-                None => panic!("field is not as string"),
-            };
-            let cmd = &e.1["cmd"].as_str().unwrap().to_string();
-            let stdout = &e.1["stdout"].as_str().unwrap().to_string();
-            let stderr = &e.1["stderr"].as_str().unwrap().to_string();
-            prog_conf.insert(name, ProgramConfig { cmd: cmd.to_string(), stdout: stdout.to_string(), stderr: stderr.to_string() });
-        }
+    for e in programs.into_iter() {
+        prog_conf.insert(get_name(e), ProgramConfig {
+            cmd: get_field(e, "cmd"),
+            stdout: get_field(e, "stdout"),
+            stderr: get_field(e, "stderr"),
+        });
     }
     return prog_conf
 }
