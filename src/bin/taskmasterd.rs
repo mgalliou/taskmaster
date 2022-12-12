2ad60891a018499bfa6f80c13af86c87f4717b2d
mgalliou <matthias.galliou@gmail.com>
2022-12-12 07:58:12 +0100

    daemon: handle first arg as config file path

--- src/bin/taskmasterd.rs
+++ src/bin/taskmasterd.rs
@@ -1,18 +1,24 @@
 extern crate yaml_rust;
 use std::collections::HashMap;
+use std::env;
 use std::os::unix::net::UnixListener;
 use taskmaster::cfg::{ConfigError, Config};
 use taskmaster::daemon::Daemon;
 
 fn main() -> Result<(), ConfigError> {
     //TODO: get config path from argv
+    let args = env::args().collect::<Vec<String>>();
+    let cfg_path = match args.get(1) {
+        Some(a) => a,
+        None => "taskmaster.yaml",
+    };
     let path = "taskmaster.socket";
     if std::fs::metadata(path).is_ok() {
         println!("A socket is already present. Deleting...");
         std::fs::remove_file(path).expect("could not delete previous socket at {:?}");
     }
     let mut daemon: Daemon = Daemon {
-        conf: Config::from_file("cfg/good/cat.yaml")?,
+        conf: Config::from_file(cfg_path)?,
         listener: UnixListener::bind(path).expect("failed to open stream"),
         proc_list: HashMap::new(),
     };
